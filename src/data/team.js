027ec75df01480afdae33feba5cccff1c70033cb
egostixcorp <initcompany0@gmail.com>
2026-01-14 18:28:44 +0530

    update

--- src/data/team.js
+++ src/data/team.js
@@ -1,14 +1,14 @@
 export const Team = [
   {
-    name: "Titas Ghosh",
-    photo: "/team/titas.png",
-    designation: "Co-founder",
-    twitter: "https://x.com/thetitasghosh",
-  },
-  {
     name: "Arun Biswas",
     photo: "/team/arun.png",
-    designation: "Co-founder",
+    designation: "CEO & Co-founder",
     twitter: "https://x.com/aruunroy",
+  },
+  {
+    name: "Titas Ghosh",
+    photo: "/team/titas.png",
+    designation: "CTO & Co-founder",
+    twitter: "https://x.com/thetitasghosh",
   },
 ];
